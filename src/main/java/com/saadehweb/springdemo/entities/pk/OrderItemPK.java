9717e3705114eff1cf8f45f7e4e5559058848ed3
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-03-20 19:32:24 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/saadehweb/springdemo/entities/pk/OrderItemPK.java
@@ -0,0 +1,53 @@
+package com.saadehweb.springdemo.entities.pk;
+
+import com.saadehweb.springdemo.entities.Order;
+import com.saadehweb.springdemo.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+//    @Serial
+//    private static final long serialVersionUID = 1L
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
