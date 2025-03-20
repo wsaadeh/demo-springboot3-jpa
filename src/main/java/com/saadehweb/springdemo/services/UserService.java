2218d75f2fe18aa2051d0fd72785ae6356f0f75f
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-03-19 21:35:44 -0300

    Service layer component registration

---
+++ src/main/java/com/saadehweb/springdemo/services/UserService.java
@@ -0,0 +1,25 @@
+package com.saadehweb.springdemo.services;
+
+import com.saadehweb.springdemo.entities.User;
+import com.saadehweb.springdemo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
