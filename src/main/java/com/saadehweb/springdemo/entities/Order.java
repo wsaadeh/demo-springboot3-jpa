92d72ce6f5fe45107f3523eb7eddd6036d8ee321
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-03-21 19:39:48 -0300

    Subtotal & Total methods

--- src/main/java/com/saadehweb/springdemo/entities/Order.java
+++ src/main/java/com/saadehweb/springdemo/entities/Order.java
@@ -93,6 +93,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal(){
+        double sum = 0.0;
+        for (OrderItem item: items){
+            sum += item.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
--- src/main/java/com/saadehweb/springdemo/entities/OrderItem.java
+++ src/main/java/com/saadehweb/springdemo/entities/OrderItem.java
@@ -66,6 +66,10 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    public Double getSubTotal(){
+        return price * quantity;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
