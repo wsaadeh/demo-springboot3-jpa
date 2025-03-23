0b105d348911ce1ecafa49264cca3cef52bf8e29
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-03-23 09:40:00 -0300

    Exception handling - findById

--- src/main/java/com/saadehweb/springdemo/services/UserService.java
+++ src/main/java/com/saadehweb/springdemo/services/UserService.java
@@ -2,6 +2,7 @@ package com.saadehweb.springdemo.services;
 
 import com.saadehweb.springdemo.entities.User;
 import com.saadehweb.springdemo.repositories.UserRepository;
+import com.saadehweb.springdemo.services.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
