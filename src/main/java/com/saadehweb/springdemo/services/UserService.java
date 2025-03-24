54db8fad0b6b6e08962b77ea0a97539d18ac28dc
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-03-24 10:49:04 -0300

    Exception handling - update

--- src/main/java/com/saadehweb/springdemo/services/UserService.java
+++ src/main/java/com/saadehweb/springdemo/services/UserService.java
@@ -4,6 +4,7 @@ import com.saadehweb.springdemo.entities.User;
 import com.saadehweb.springdemo.repositories.UserRepository;
 import com.saadehweb.springdemo.services.exception.DatabaseException;
 import com.saadehweb.springdemo.services.exception.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -46,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id,User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
